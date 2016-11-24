cb54b7c585a31ee1c1386083f01e9e5afa62c456
gtd4 <gtd005@gmail.com>
2016-11-25 10:16:03 +1300

    More angular magic, new directives, controllers and forms

---
+++ AngularPractice/Scripts/app.js
@@ -0,0 +1,103 @@
+﻿(function () {
+    var app = angular.module('store', []);
+    app.controller('StoreController', function () {
+        this.products = gems;
+    });
+
+    var gems =
+        [
+        {
+            name: 'Dodecahedron',
+            price: 2.95,
+            description: "Twelve sided Gem",
+            canPurchase: true,
+            soldOut: false,
+            images: [
+                {
+                    full: "https://unsplash.it/200/200?random",
+                    thumb: "https://unsplash.it/100/100?random",
+                },
+                {
+                    full: "https://unsplash.it/200/200?random",
+                    thumb: "https://unsplash.it/100/100?random",
+                },
+                {
+                    full: "https://unsplash.it/200/200?random",
+                    thumb: "https://unsplash.it/100/100?random",
+                },
+            ],
+            reviews: [
+                {
+                    stars: 1,
+                    body: "This product Sucks",
+                    author: "ggg@ggg.com"
+                },
+                {
+                    stars: 4,
+                    body: "This product is awesome",
+                    author: "ggg@ggg.com"
+                },
+                {
+                    stars: 5,
+                    body: "This product was great",
+                    author: "ggg@ggg.com"
+                },
+
+            ],
+        },
+         {
+             name: 'Tri',
+             price: 90.95,
+             description: "3 sided Gem",
+             canPurchase: true,
+             soldOut: false,
+             images: [
+                {
+                    full: "https://unsplash.it/200/200?random",
+                    thumb: "https://unsplash.it/100/100?random",
+                },
+                {
+                    full: "https://unsplash.it/200/200?random",
+                    thumb: "https://unsplash.it/100/100?random",
+                },
+                {
+                    full: "https://unsplash.it/200/200?random",
+                    thumb: "https://unsplash.it/100/100?random",
+                },
+             ]
+         },
+          {
+              name: 'Diamond',
+              price: 2.95,
+              description: "4 sided Gem",
+              canPurchase: true,
+              soldOut: false,
+              images: [
+                {
+                    full: "https://unsplash.it/200/200?random",
+                    thumb: "https://unsplash.it/100/100?random",
+                },
+                {
+                    full: "https://unsplash.it/200/200?random",
+                    thumb: "https://unsplash.it/100/100?random",
+                },
+                {
+                    full: "https://unsplash.it/200/200?random",
+                    thumb: "https://unsplash.it/100/100?random",
+                },
+              ]
+          },
+        ];
+
+    app.controller("PanelController", function () {
+        this.tab = 1;
+        this.selectTab = function (setTab) {
+            this.tab = setTab;
+        }
+        this.isSelected = function (checkTab) {
+            return this.tab === checkTab;
+        }
+    });
+
+})();
+
